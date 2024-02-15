88836f043b9ed4608ed729a3e0647e3125890e5f
siggu <siggu100@gmail.com>
2024-02-15 20:23:28 +0900

    move files

---
+++ wishlists/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from rooms.serializers import RoomListSerializer
+from .models import Wishlist
+
+
+class WishlistSerializer(ModelSerializer):
+    rooms = RoomListSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Wishlist
+        fields = (
+            "pk",
+            "name",
+            "rooms",
+        )
