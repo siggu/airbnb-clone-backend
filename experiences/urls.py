f25f6bcc169edabd5d4ad55f8484d353fc1c4368
siggu <siggu100@gmail.com>
2024-02-15 21:12:06 +0900

    gitignore

--- experiences/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.Experiences.as_view()),
-    path("perks/", views.Perks.as_view()),
-    path("perks/<int:pk>", views.PerkDetail.as_view()),
-]
