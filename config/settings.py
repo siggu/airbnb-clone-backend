1d0ecf99a08816f15ca220aa85d11478678aa3d2
siggu <siggu100@gmail.com>
2024-02-16 18:41:25 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = [
     "localhost",
+    "backend.mokugyo.biz",
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
@@ -191,8 +192,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
-    CORS_ALLOWED_ORIGINS = ["https://airbnb-frontend-sybk.onrender.com"]
-    CSRF_TRUSTED_ORIGINS = ["https://airbnb-frontend-sybk.onrender.com"]
+    CORS_ALLOWED_ORIGINS = ["https://mokugyo.biz"]
+    CSRF_TRUSTED_ORIGINS = ["https://mokugyo.biz"]
 
 CORS_ALLOW_CREDENTIALS = True
 
