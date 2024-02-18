129f0bb13059ee3811d72db1d081a45c56a6c600
siggu <siggu100@gmail.com>
2024-02-18 18:35:00 +0900

    One Time Upload

--- medias/urls.py
+++ medias/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PhotoDetail
+from .views import PhotoDetail, GetUploadURL, UploadPhoto
 
 urlpatterns = [
+    path("photos/get-url", GetUploadURL.as_view()),
+    path("photos/upload-photo", UploadPhoto.as_view()),
     path("photos/<int:pk>", PhotoDetail.as_view()),
 ]
--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,7 @@
+import requests
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK
@@ -23,3 +27,30 @@ class PhotoDetail(APIView):
             raise PermissionDenied
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+
+class GetUploadURL(APIView):
+    def post(self, request):
+        url = f"https://api.cloudflare.com/client/v4/accounts/${settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests(
+            url,
+            headers={"Authorization": f"Bearer {settings.CF_TOKEN}"},
+        )
+        one_time_url = one_time_url.json()
+        result = one_time_url("result")
+        if result:
+            return Response({"uploadURL": result.get("uploadURL")})
+        else:
+            return Response(
+                {
+                    "id": "00000",
+                    "uploadURL": "http://127.0.0.1:8000/api/v1/medias/photos/upload-photo",
+                }
+            )
+
+
+class UploadPhoto(APIView):
+    def post(self, request):
+        image_data = request.FILES["file"]
+        path = default_storage.save(image_data, ContentFile(image_data.read()))
+        return Response({"result": {"id": path}})
